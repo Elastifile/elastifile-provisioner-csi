0f0474a496877b08ce769e4348e6550379897d26
Jean Spector <jean.spector@elastifile.com>
2018-12-27 10:58:47 +0200

    Added teardown in deployrunner

--- deployrunner/run-deploy.sh
+++ deployrunner/run-deploy.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
+# Usage:
+# To deploy ECFS CSI plugin:
+#   1. Edit deploy-runner-pod.yaml to include parameters specific to your ECFS cluster, e.g. Elastifile Management Console address
+#   2. ./run-deploy.sh
+# To tear down the plugin, run:
+#   1. RUN_TEARDOWN=true ./run-deploy.sh
+
 # Default values
-: ${DRY_RUN:=false}
 : ${CREATE_MANIFESTS:=true}
 : ${RUN_DEPLOY:=true}
+: ${RUN_TEARDOWN:=false}
 : ${TEARDOWN_MANIFESTS:=true}
-
 
 MYNAME=$(basename $0)
 MYPATH=$(dirname $0)
 DEPLOYMENT_BASE=${MYPATH}
-
-DRY_RUN_FLAG=""
-if ${DRY_RUN}; then
-    echo "WARNING: DRY RUN"
-    DRY_RUN_FLAG="--dry-run"
-fi
 
 MANIFESTS=(cluster-admin-rbac.yaml)
 
@@ -25,7 +25,7 @@ if which gcloud > /dev/null 2>&1; then
     CRB_NAME=default-admin
     if ! kubectl get clusterrolebinding ${CRB_NAME} > /dev/null 2>&1; then
         echo "Creating clusterrolebinding"
-        kubectl create clusterrolebinding ${CRB_NAME} --clusterrole=cluster-admin --user=$(gcloud config get-value account) ${DRY_RUN_FLAG}
+        kubectl create clusterrolebinding ${CRB_NAME} --clusterrole=cluster-admin --user=$(gcloud config get-value account)
     fi
 fi
 
@@ -44,30 +44,34 @@ kubectl config view --minify --flatten > ${TMP_KUBE_CONFIG}
 if ${CREATE_MANIFESTS}; then
     for MANIFEST in ${MANIFESTS[@]}; do
         echo "Creating ${MANIFEST}"
-        kubectl create -f "${DEPLOYMENT_BASE}/${MANIFEST}" ${DRY_RUN_FLAG}
+        kubectl create -f "${DEPLOYMENT_BASE}/${MANIFEST}"
     done
 fi
 
 # Run the deploy job
-DEPLOY_MANIFEST="${DEPLOYMENT_BASE}/deploy-runner-pod.yaml"
-if ${RUN_DEPLOY}; then
+DEPLOY_POD_MANIFEST="${DEPLOYMENT_BASE}/deploy-runner-pod.yaml"
+if ${RUN_DEPLOY} || ${RUN_TEARDOWN}; then
     echo "Running containerized deploy script"
     # Note: the contents of this manifest include environment variables required to properly deploy the plugin
-    kubectl create -f ${DEPLOY_MANIFEST} ${DRY_RUN_FLAG}
-    kubectl wait --for=condition=Ready -f ${DEPLOY_MANIFEST} ${DRY_RUN_FLAG} --timeout=1m
+    kubectl create -f ${DEPLOY_POD_MANIFEST}
+    kubectl wait --for=condition=Ready -f ${DEPLOY_POD_MANIFEST} --timeout=1m
 
     kubectl cp ${TMP_KUBE_CONFIG} default/deploy-runner:/root/.kube/config
-    kubectl exec -it deploy-runner deploy/deploy-plugin.sh
+    if ${RUN_TEARDOWN}; then
+        kubectl exec -it deploy-runner deploy/teardown-plugin.sh
+    else
+        kubectl exec -it deploy-runner deploy/deploy-plugin.sh
+    fi
 
-    kubectl delete -f ${DEPLOY_MANIFEST} ${DRY_RUN_FLAG}
+    kubectl delete -f ${DEPLOY_POD_MANIFEST}
 fi
 
 # Teardown
 if ${TEARDOWN_MANIFESTS}; then
     for MANIFEST in ${MANIFESTS[@]}; do
         echo "Deleting ${MANIFEST}"
-        kubectl delete -f "${DEPLOYMENT_BASE}/${MANIFEST}" ${DRY_RUN_FLAG}
+        kubectl delete -f "${DEPLOYMENT_BASE}/${MANIFEST}"
     done
-    kubectl delete -f ${DEPLOY_MANIFEST} ${DRY_RUN_FLAG} > /dev/null 2>&1
+    kubectl delete -f ${DEPLOY_POD_MANIFEST} > /dev/null 2>&1
     kubectl delete clusterrolebinding ${CRB_NAME} > /dev/null 2>&1
 fi
