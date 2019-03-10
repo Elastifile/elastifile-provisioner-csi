d943281b7bf19a7a39412bf51dca8bd967c85d8b
Jean Spector <jean.spector@elastifile.com>
2019-03-10 14:25:01 +0200

    gke-deploy.sh - Switched to safe_create functions that won't fail is the resource exists

--- gke-deploy/gke-deploy.sh
+++ gke-deploy/gke-deploy.sh
@@ -6,16 +6,40 @@ MYPATH=$(dirname $0)
 
 source ${MYPATH}/functions.sh
 
-# Create cluster-wide resources, which are not supported by GKE schema.yaml method
+function safe_create () {
+    manifest="$1"
+    exec_cmd kubectl delete -f "${manifest}" --namespace ${NAMESPACE}
+    assert_cmd kubectl create -f "${manifest}" --namespace ${NAMESPACE}
 
-NAMESPACE=${NAMESPACE} envsubst < "${MYPATH}/templates/storageclass.yaml" | kubectl create -f -
-if [[ $? != 0 ]]; then
-    echo "Failed to create StorageClass"
-    exit 1
-fi
+}
 
-assert_cmd kubectl create -f "${MYPATH}/snapshotclass.yaml" --namespace ${NAMESPACE}
-assert_cmd kubectl create -f "${MYPATH}/csidriver.yaml"
-assert_cmd kubectl create -f "${MYPATH}/csinodeinfo.yaml"
+function safe_create_template () {
+    manifest="$1"
+    NAMESPACE=${NAMESPACE} envsubst < "${manifest}" | kubectl delete -f -
+    NAMESPACE=${NAMESPACE} envsubst < "${manifest}" | kubectl create -f -
+    if [[ $? != 0 ]]; then
+        echo "Failed to create StorageClass"
+        exit 1
+    fi
+}
+
+# Cluster-wide resources are not supported by GKE schema.yaml method - create them here
+
+safe_create_template "${MYPATH}/templates/storageclass.yaml"
+
+safe_create "${MYPATH}/snapshotclass.yaml"
+safe_create "${MYPATH}/csidriver.yaml"
+safe_create "${MYPATH}/csinodeinfo.yaml"
+
+#NAMESPACE=${NAMESPACE} envsubst < "${MYPATH}/templates/storageclass.yaml" | kubectl delete -f -
+#NAMESPACE=${NAMESPACE} envsubst < "${MYPATH}/templates/storageclass.yaml" | kubectl create -f -
+#if [[ $? != 0 ]]; then
+#    echo "Failed to create StorageClass"
+#    exit 1
+#fi
+
+#assert_cmd kubectl create -f "${MYPATH}/snapshotclass.yaml" --namespace ${NAMESPACE}
+#assert_cmd kubectl create -f "${MYPATH}/csidriver.yaml"
+#assert_cmd kubectl create -f "${MYPATH}/csinodeinfo.yaml"
 
 echo "Manifests created"
