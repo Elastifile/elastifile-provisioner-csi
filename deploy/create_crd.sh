2b902496b9cca259052420f333aa6b27b3e980cd
Jean Spector <jean.spector@elastifile.com>
2019-04-10 20:52:11 +0300

    Updated CSI CRDs from https://github.com/kubernetes/kubernetes/blob/release-1.13/cluster/addons/storage-crds

--- deploy/create_crd.sh
+++ deploy/create_crd.sh
@@ -4,5 +4,7 @@ MYPATH=$(dirname $0)
 
 source ${MYPATH}/functions.sh
 
+# Obtained from https://github.com/kubernetes/kubernetes/blob/release-1.13/cluster/addons/storage-crds
+# Not needed as of K8s 1.14
 assert_cmd kubectl create -f csidriver.yaml --validate=false
 assert_cmd kubectl create -f csinodeinfo.yaml --validate=false
