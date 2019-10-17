0ab5080b101b259823b4a2829ea92aeb3aff99f4
Jean Spector <jeanspector@google.com>
2019-10-17 11:41:54 +0300

    Updated default GKE cluster creation example to use K8s 1.14

--- examples/create_gke_cluster.sh
+++ examples/create_gke_cluster.sh
@@ -3,7 +3,7 @@
 # Usage:
 # GCP_PROJECT_ID=399 create_gke_cluster.sh
 
-: ${K8S_VERSION:="1.13.5-gke.10"}
+: ${K8S_VERSION:="1.14.6"}
 # Alpha versions can be found at https://cloud.google.com/kubernetes-engine/docs/release-notes
 
 : ${GKE_CLUSTER_NAME:="cluster-1"}
@@ -16,7 +16,7 @@
 : ${GCP_SUBNETWORK:="projects/${GCP_PROJECT_NAME}/regions/${GCP_REGION}/subnetworks/${GKE_VPC}"}
 
 : ${GKE_CLUSTER_USER:="admin"}
-: ${GKE_NODE_VM:="custom-1-1536"}
+: ${GKE_NODE_VM:="n1-standard-1"}
 : ${GKE_NODE_IMAGE:="COS"} # Can be COS|UBUNTU
 : ${GKE_NODE_DISK_TYPE:="pd-standard"}
 : ${GKE_NODE_DISK_SIZE:="50"} # GB
@@ -29,5 +29,5 @@ fi
 
 set -x
 EXPERIMENTAL_FLAGS="--no-issue-client-certificate --no-enable-ip-alias --metadata disable-legacy-endpoints=true --no-enable-autoupgrade --no-enable-autorepair --enable-kubernetes-alpha"
-gcloud beta container --project ${GCP_PROJECT_NAME} clusters create ${GKE_CLUSTER_NAME} --zone ${GCP_ZONE} --username ${GKE_CLUSTER_USER} --cluster-version ${K8S_VERSION} --machine-type ${GKE_NODE_VM} --image-type ${GKE_NODE_IMAGE} --disk-type ${GKE_NODE_DISK_TYPE} --disk-size ${GKE_NODE_DISK_SIZE} --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes ${GKE_NODE_COUNT} --enable-cloud-logging --enable-cloud-monitoring --network ${GCP_NETWORK} --subnetwork ${GCP_SUBNETWORK} --addons HorizontalPodAutoscaling,HttpLoadBalancing,KubernetesDashboard ${EXPERIMENTAL_FLAGS}
+gcloud beta container --project ${GCP_PROJECT_NAME} clusters create ${GKE_CLUSTER_NAME} --zone ${GCP_ZONE} --username ${GKE_CLUSTER_USER} --cluster-version ${K8S_VERSION} --machine-type ${GKE_NODE_VM} --image-type ${GKE_NODE_IMAGE} --disk-type ${GKE_NODE_DISK_TYPE} --disk-size ${GKE_NODE_DISK_SIZE} --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes ${GKE_NODE_COUNT} --enable-stackdriver-kubernetes --network ${GCP_NETWORK} --subnetwork ${GCP_SUBNETWORK} --addons HorizontalPodAutoscaling,HttpLoadBalancing ${EXPERIMENTAL_FLAGS}
 set +x
